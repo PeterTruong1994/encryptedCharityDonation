7551d1e2ced9429554601c07ef48dc9a8209948b
PeterTruong1994 <truonghonglinh1994@gmail.com>
2025-09-27 09:30:04 +0700

    create smartcontract

---
+++ abi/EncryptedCharityDonationAddresses.ts
@@ -0,0 +1,9 @@
+
+/*
+  This file is auto-generated.
+  By commands: 'npx hardhat deploy' or 'npx hardhat node'
+*/
+export const FHECounterAddresses = { 
+  "11155111": { address: "0xBD6eaf2e81c645aE9c30371C4fbe5A81DaF38cEe", chainId: 11155111, chainName: "sepolia" },
+  "31337": { address: "0x7553CB9124f974Ee475E5cE45482F90d5B6076BC", chainId: 31337, chainName: "hardhat" },
+};
